5ba96cc92440392fd5b045088848373de4b9d1de
Yuanjun Zhong <zhuimengzyj@gmail.com>
2017-11-07 15:15:46 +0800

    dev: 添加开发者服务器
    添加热加载
    添加自动开启浏览器
    
    Changes to be committed:
    	new file:   .gitignore
    	new file:   gulpfile.js
    	new file:   package-lock.json
    	new file:   package.json
    	new file:   public/index.html

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp'),
+    connect = require('gulp-connect'),
+    open = require('gulp-open'),
+    path = require('path');
+
+const PORT = 8010;
+
+gulp.task('html', () => {
+    gulp.src('./public/**/*.html')
+        .pipe(connect.reload());
+});
+
+gulp.task('style', () => {
+    gulp.src('./public/css/**/*.css')
+        .pipe(connect.reload());
+});
+
+gulp.task('server', () => {
+    connect.server({
+        port: PORT,
+        root: path.resolve('./public'),
+        livereload: true
+    });
+    gulp.src(__filename).pipe(open({
+        uri: `http://localhost:${PORT}`
+    }));
+    gulp.watch('./public/**/*.html', ['html']);
+    gulp.watch('./public/css/**/*.css', ['style']);
+});
